f80b9fa1f4bb8b9f8a9bf0640d5c03be2e6b0ab8
robinbenitezmora <robinbenitez1990@gmail.com>
2022-08-02 13:53:55 -0500

    Add html, css and javascript files with code lines as starting the project with classes in javascript

--- main.js
+++ main.js
@@ -1,1 +1,71 @@
+class Library {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
+class InterfaceUser {
+  static displayBooks() {
+    const books = [
+      {
+        title: 'Nuremberg Process',
+        author: 'Joe J. Heidecker',
+      },
+      {
+        title: 'The Science Fiction Hall of Fame',
+        author: 'Isaac Asimov',
+      },
+      {
+        title: 'The Martian',
+        author: 'Andy Weir',
+      },
+    ];
+    books.forEach((book) => { InterfaceUser.addBookToLibrary(book); });
+  }
+
+  static addBookToLibrary(book) {
+    const library = document.getElementById('book_list');
+    const row = document.createElement('section');
+
+    row.innerHTML = `
+      <div class='book'>
+        <div class="title">'${book.title}' ${'by'} ${book.author}</div>
+        <div><button href="#" class='delete'>Remove</button></div><hr>
+      </div>
+    `;
+
+    library.appendChild(row);
+  }
+
+  static deleteBook(element) {
+    if (element.classList.contains('delete')) {
+      element.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', InterfaceUser.displayBooks);
+
+document.getElementById('books__add').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  if (title === '' || author === '') {
+    alert('Please fill in all fields');
+  } else {
+    const book = new Library(title, author);
+    InterfaceUser.addBookToLibrary(book);
+    InterfaceUser.clearFields();
+  }
+});
+
+document.getElementById('book_list').addEventListener('click', (e) => {
+  InterfaceUser.deleteBook(e.target);
+});
